39777428e6c62616b0a90b957a685499f36ac702
Florian Fankhauser <flo@lingu.at>
2019-01-27 08:21:31 +0100

    Add docs

--- envvar.go
+++ envvar.go
@@ -1,3 +1,5 @@
+// Package envvar provides functions to access typed
+// environment variables and define default values.
 package envvar
 
 import (
@@ -6,24 +8,34 @@ import (
 	"strconv"
 )
 
+// DefineString defines a string environment variable
+// with a defaultValue.
 func DefineString(name string, defaultValue string) {
 	if _, exists := os.LookupEnv(name); !exists {
 		os.Setenv(name, defaultValue)
 	}
 }
 
+// DefineInt defines an int environment variable
+// with a defaultValue.
 func DefineInt(name string, defaultValue int) {
 	if _, exists := os.LookupEnv(name); !exists {
 		os.Setenv(name, strconv.Itoa(defaultValue))
 	}
 }
 
+// DefineBool defines a boolean environment variable
+// with a defaultValue.
 func DefineBool(name string, defaultValue bool) {
 	if _, exists := os.LookupEnv(name); !exists {
 		os.Setenv(name, strconv.FormatBool(defaultValue))
 	}
 }
 
+// GetString returns the value of the environment
+// variable with the given name. If the environment
+// variable is not set and no defaultValue was defined,
+// this func panics.
 func GetString(name string) string {
 	v := os.Getenv(name)
 	if v == "" {
@@ -32,6 +44,10 @@ func GetString(name string) string {
 	return v
 }
 
+// GetInt returns the value of the environment
+// variable with the given name converted to int.
+// If the environment variable is not set and no
+// defaultValue was defined, this func panics.
 func GetInt(name string) int {
 	s := GetString(name)
 	i, err := strconv.Atoi(s)
@@ -41,6 +57,10 @@ func GetInt(name string) int {
 	return i
 }
 
+// GetBool returns the value of the environment
+// variable with the given name converted to bool.
+// If the environment variable is not set and no
+// defaultValue was defined, this func panics.
 func GetBool(name string) bool {
 	s := GetString(name)
 	b, err := strconv.ParseBool(s)
